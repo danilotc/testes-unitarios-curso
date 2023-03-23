a48d351abfa6803b7eefce5fbfce4485e495cc91
Danilo Trindade <danilotrindade87@gmail.com>
2023-03-22 23:19:17 -0300

    Teste sem ferramenta
    Realizando teste sem a utilizacao de ferramenta

--- src/main/java/edu/testesunitarios/servicos/LocacaoService.java
+++ src/main/java/edu/testesunitarios/servicos/LocacaoService.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import edu.testesunitarios.entidades.Filme;
 import edu.testesunitarios.entidades.Locacao;
 import edu.testesunitarios.entidades.Usuario;
+import edu.testesunitarios.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -30,9 +31,16 @@ public class LocacaoService {
 	
 	public static void main(String[] args) {
 		//cenario
-		
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
 		//acao
-		
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
 		//verificacao
+		System.out.println(locacao.getValor() == 5.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
