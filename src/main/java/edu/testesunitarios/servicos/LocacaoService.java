2e5b9d945541d1f0cc05c4e266df3316aa7f1f80
Danilo Trindade <danilotrindade87@gmail.com>
2023-03-23 23:26:37 -0300

    Organizando os arquivos de teste
    Uma nova classe foi criada para realizar o teste e esta classe esta
    organizada em uma estrutura de pasta igual a estrutura onde o arquivo
    fonte esta localizado.
    
    Em tempo de execucao o arquivo LocacaoServiceTest sera enxergado como se
    estivesse no mesmo pacote do arquivo LocacaoService. Esta separado assim
    porque estes arquivos de teste nao sao enviados para producao.
    
    Alem disso, existe toda uma questao de visualizacacao de metodos e
    atributos em relacao a essa organizacao de pacotes.

--- src/main/java/edu/testesunitarios/servicos/LocacaoService.java
+++ src/main/java/edu/testesunitarios/servicos/LocacaoService.java
@@ -4,13 +4,9 @@ import static edu.testesunitarios.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import edu.testesunitarios.entidades.Filme;
 import edu.testesunitarios.entidades.Locacao;
 import edu.testesunitarios.entidades.Usuario;
-import edu.testesunitarios.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -30,21 +26,5 @@ public class LocacaoService {
 		//TODO adicionar método para salvar
 		
 		return locacao;
-	}
-	
-	@Test
-	public void teste() {
-		//cenario
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
-
-		//acao
-		Locacao locacao = service.alugarFilme(usuario, filme);
-
-		//verificacao
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
---
+++ src/test/java/edu/testesunitarios/service/LocacaoServiceTest.java
@@ -0,0 +1,31 @@
+package edu.testesunitarios.service;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.testesunitarios.entidades.Filme;
+import edu.testesunitarios.entidades.Locacao;
+import edu.testesunitarios.entidades.Usuario;
+import edu.testesunitarios.servicos.LocacaoService;
+import edu.testesunitarios.utils.DataUtils;
+
+public class LocacaoServiceTest {
+
+	@Test
+	public void teste() {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		//acao
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		//verificacao
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
