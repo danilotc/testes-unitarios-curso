1949d66ff651ca5d933eb94ba91c1ab3c631841b
Danilo Trindade <danilotrindade87@gmail.com>
2023-03-30 23:25:24 -0300

    Tratamento de excecao - parte 3
    Esta implementacao mostra como criar um teste que esta esperando uma
    excecao. Para o teste passar uma excecao precisa ser lancada porque o
    teste esta esperando essa excecao.
    
    Aqui a anotacao @Test recebe um argumento indicando qual excecao esta
    sendo esperada, neste caso, Exception.
    
    @Test(expected = Exception.class)

--- src/test/java/edu/testesunitarios/service/LocacaoServiceTest.java
+++ src/test/java/edu/testesunitarios/service/LocacaoServiceTest.java
@@ -36,4 +36,20 @@ public class LocacaoServiceTest {
 		erro.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		erro.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 	}
+	
+	/*
+	 * Se uma excecao eh lancada e este teste funcionar o log de failure trace 
+	 * nao mostra nenhuma informacao util de rastreamento alem da exceptativa 
+	 * de receber uma Exception.
+	 */
+	@Test(expected = Exception.class)
+	public void testLocacao_filmeSemEstoque() throws Exception {
+		//cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 0, 5.0);
+
+		//acao
+		service.alugarFilme(usuario, filme);
+	}
 }
